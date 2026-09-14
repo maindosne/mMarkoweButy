7e1c137e18b6e85e21f229e3c598cf55ae9e55f3
maindosne <marrkke93@gmail.com>
2026-09-14 11:06:33 +0200

    Bump admin app to 1.2.2 after AUTO fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pl.mmarkowebuty.admin"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
     }
 
     buildTypes {
