4ceae312eab8e0e6473f39727fac2e3cb446c771
maindosne <marrkke93@gmail.com>
2026-09-18 12:43:52 +0200

    Bump admin app to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pl.mmarkowebuty.admin"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.5.0"
+        versionCode = 9
+        versionName = "1.5.1"
     }
 
     buildTypes {
